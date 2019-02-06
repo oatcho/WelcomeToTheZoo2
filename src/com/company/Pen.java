e528267d3dfe7398cdcbaec5c54b2af804597402
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-02-06 12:20:00 -0500

    tried to work on like 3 things at once, but nothing really works

--- src/com/company/Pen.java
+++ src/com/company/Pen.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pen extends BabyAnimal {
 
@@ -9,7 +10,50 @@ public class Pen extends BabyAnimal {
 
     public Pen () {
 
-    };
+    }
 
+    public void createNewPen(Pen newPen){
+        ArrayList<Animal> nameOfPen = new ArrayList<>();
+    }
+
+    public void createNewBabyPen(Pen newPen2){
+        ArrayList<BabyAnimal> nameOfBabyPen = new ArrayList<>();
+    }
+
+    public void addAnimal(Animal newAnimal){
+        animalArrayList.add(newAnimal);
+    }
+
+    public void addBabyAnimal(BabyAnimal newBabyAnimal){
+        babyAnimalArrayList.add(newBabyAnimal);
+    }
+
+    public void removeAnimal(String species){
+        animalArrayList.remove(searchBySpecies(species));
+    }
+
+    public void removeBabyAnimal(String species){
+        babyAnimalArrayList.remove(searchByBabySpecies(species));
+    }
+
+    public Animal searchBySpecies(String species){
+        Animal a = new Animal();
+        for (int i = 0; i < animalArrayList.size(); i++){
+            if(animalArrayList.get(i).getSpecies() == species){
+                a = animalArrayList.get(i);
+            }
+
+        }
+        return a;
+    }
+
+    public BabyAnimal searchByBabySpecies(String species){
+        BabyAnimal b = new BabyAnimal();
+        for (int j = 0; j < babyAnimalArrayList.size(); j++){
+            if(babyAnimalArrayList.get(j).getSpecies() == species){
+                b = babyAnimalArrayList.get(j);
+            }
+        } return b;
+    }
 
 }
--- src/com/company/ZooDemo.java
+++ src/com/company/ZooDemo.java
@@ -8,6 +8,7 @@ public class ZooDemo {
 
         Scanner key = new Scanner(System.in);
         boolean menuLoop = true;
+        Pen newPen = new Pen();
 
         System.out.println("Welcome to the Zoo! What would you like to do?");
 
@@ -17,14 +18,61 @@ public class ZooDemo {
             int userOption = key.nextInt();
             switch(userOption){
                 case 1:
+                    //I don't think any of this case is correct or working how I want... moving on to other functions, will come back
+                    System.out.println("1) Add Adult Animal Pen \n2) Add Baby Animal Pen \n3) Remove Pen");
+                    int addOrRemovePenOption = key.nextInt();
+                    if(addOrRemovePenOption == 1){
+                        Pen newPenName = new Pen();
+                    newPenName.createNewPen(newPenName);}
+                    else if (addOrRemovePenOption ==2){
+                        Pen newBabyPenName = new Pen();
+                        newBabyPenName.createNewBabyPen(newBabyPenName);
+                    }
+                    else if (addOrRemovePenOption == 3) {
+                        //add remove penarray function here, unsure of a few things, will come back
+                    } else {
+                        System.out.println("Incorrect Response");
+                    }
                     break;
                 case 2:
+                    System.out.println("1) Add Animal to Animal Pen \n 2) Add Baby Animal to Baby Animal Pen \n 3) Remove Animal from Pen \n 4) Remove Baby Animal from Pen");
+                    int userOption2 = key.nextInt();
+                    if(userOption2 == 1){
+                        System.out.println("Please enter Animal Species:");
+                        key.nextLine();
+                        String species = key.nextLine();
+                        System.out.println("Please enter Animal Size:");
+                        String size = key.nextLine();
+                        System.out.println("Please enter Animal Gender:");
+                        String gender = key.nextLine();
+                        Animal newAnimal = new Animal(species, size, gender);
+                        newPen.addAnimal(newAnimal);
+                    } else if (userOption2 == 2){
+                        System.out.println("Please enter Baby Animal Species:");
+                        key.nextLine();
+                        String species = key.nextLine();
+                        System.out.println("Please enter Baby Animal Size:");
+                        String size = key.nextLine();
+                        System.out.println("Please enter Baby Animal Gender:");
+                        String gender = key.nextLine();
+                        System.out.println("Please enter Baby Animal Birthday");
+                        String birthday = key.nextLine();
+                        BabyAnimal newBabyAnimal = new BabyAnimal();
+                        newPen.addBabyAnimal(newBabyAnimal);
+                    } else if (userOption2 == 3){
+                        System.out.println("What Species of Animal would you like to remove?");
+                        String searchedForSpecies = key.nextLine();
+
+
+
+                    }
                     break;
                 case 3:
                     break;
                 case 4:
                     break;
                 case 5:
+                    menuLoop = false;
                     break;
                 default:
                     System.out.println("Incorrect Response");
